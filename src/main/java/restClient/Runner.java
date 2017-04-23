af8df0a886f55824fc039f074674522bccfaa1bc
Gergana Ivanova <gergana.ivanova@qinec.com>
2017-04-23 17:15:29 +0300

    changed tags

--- src/main/java/restClient/Runner.java
+++ src/main/java/restClient/Runner.java
@@ -6,7 +6,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 public class Runner {
 	
 	@CucumberOptions(monochrome = true, format = {"pretty", "html:target/cucumber","json:target/cucumber-report.json"}, features="src/test/resources/features", 
-			tags={"@forecast"},
+			tags={"@OpenWeather"},
 			glue = "classpath:stepDef")
 	public class RunnerA extends AbstractTestNGCucumberTests { 
 		
